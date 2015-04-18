45592a09871eb56d2f04b250f82ca97bf9cd693f
count_xyz <skeptoid@gmail.com>
2015-04-18 10:58:16 -0400

    11 digit phone numbers trimmed if number starts with 1.

--- lib/phone_number.rb
+++ lib/phone_number.rb
@@ -1,18 +1,45 @@
 class PhoneNumber
 
   def initialize phone_number
-    @phone_number = phone_number
+    @phone_number = phone_number.to_s
   end
 
   def verify_phone_number
-    @phone_number.to_s =~ /\d{10}/ ? good_number_message : bad_number_message
+    if @phone_number.size > 9
+     good_number
+    else
+      bad_number_message
+    end
+  end
+
+  private
+
+  def good_number
+    @phone_number.size == 10 ? good_number_message : trim_number_verification
+  end
+
+  def trim_number_verification
+    @phone_number[0] == '1' ? trim_number : bad_number_message
+  end
+
+  def trim_number
+    eleven_digit_trim
+    trim_message
+  end
+
+  def eleven_digit_trim
+    @phone_number = @phone_number[1..10]
   end
 
   def good_number_message
-    'Good Number'
+    "#{@phone_number} is a good number"
+  end
+
+  def trim_message
+    "Number Trimmed: #{@phone_number}"
   end
 
   def bad_number_message
-    'Bad Number'
+    "#{@phone_number} is a bad phone number"
   end
 end
--- spec/lib/phone_number_spec.rb
+++ spec/lib/phone_number_spec.rb
@@ -2,11 +2,33 @@ require 'phone_number'
 
 RSpec.describe PhoneNumber do
 
-  it 'will not accept a phone number shorter than 10 digits' do
-    expect((PhoneNumber.new 123456789).verify_phone_number).to eq 'Bad Number'
+  describe 'Unacceptable Numbers' do
+    it 'will not accept a phone number shorter than 10 digits' do
+      phone_number = PhoneNumber.new 123456789
+      response     = '123456789 is a bad phone number'
+      expect(phone_number.verify_phone_number).to eq response
+    end
   end
 
-  it 'will accept phone numbers that are 10 digits' do
-    expect((PhoneNumber.new 1234567890).verify_phone_number).to eq 'Good Number'
+  describe 'Acceptable Numbers' do
+    it 'will accept phone numbers that are 10 digits' do
+      phone_number = PhoneNumber.new 1234567890
+      response     = '1234567890 is a good number'
+      expect(phone_number.verify_phone_number).to eq response
+    end
+  end
+
+  describe 'Trimmed Numbers' do
+    it 'will trim 11 digits and use last 10 when first digit is 1' do
+      phone_number = PhoneNumber.new 12345678901
+      response     = 'Number Trimmed: 2345678901'
+      expect(phone_number.verify_phone_number).to eq response
+    end
+
+    it 'will not trim 11 digits and use last 10 when first digit not 1' do
+      phone_number = PhoneNumber.new 22345678901
+      response     = '22345678901 is a bad phone number'
+      expect(phone_number.verify_phone_number).to eq response
+    end
   end
 end
